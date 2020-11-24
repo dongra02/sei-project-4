906e98b9d9924fa68a44c751dcc75a3c8e50fa57
dongra02 <dsgraham1@gmail.com>
2020-11-24 12:46:06 +0200

    test redis tls url

--- project/settings.py
+++ project/settings.py
@@ -90,7 +90,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('REDIS_URL', ('127.0.0.1', 6379))],
+            "hosts": [os.environ.get('REDIS_TLS_URL', ('127.0.0.1', 6379))],
         },
     },
 }
