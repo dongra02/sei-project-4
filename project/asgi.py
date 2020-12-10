79acde07adfc66cdb2d51d8fe2ce1a56075a5cb0
dongra02 <dsgraham1@gmail.com>
2020-12-10 15:05:58 -0500

    asgi message error

--- project/asgi.py
+++ project/asgi.py
@@ -1,8 +1,6 @@
 """
 ASGI config for project project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
@@ -11,10 +9,10 @@ import os
 import django
 from channels.routing import get_default_application
 
-# from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
+
+# from django.core.asgi import get_asgi_application
 
 # application = get_asgi_application()
 application = get_default_application()
