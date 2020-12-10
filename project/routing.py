79326aace48fb3c23c697e2c33a5b9fd400247d9
dongra02 <dsgraham1@gmail.com>
2020-12-10 14:46:01 -0500

    build updates

--- project/routing.py
+++ project/routing.py
@@ -2,6 +2,9 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat_messages.routing
 
+import os
+import django
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
